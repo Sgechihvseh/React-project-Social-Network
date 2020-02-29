8940302db1c5c627c7495393729e297e959145bc
Рома Хмарук <rkhmaruk4@gmail.com>
2020-02-29 21:25:56 +0300

    Connected axios and created get-request to the server to get users massive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import {NavLink} from "react-router-dom";
 import FriendsContainer from "../Friends/FriendsContainer";
 
 const Navbar = (props) => {
-	debugger;
 	return <nav className={s.nav}>
 		<div className={s.item}>
 			<NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,46 +1,19 @@
 import React from "react";
-import s from "./Users.module.css"
+import s from "./Users.module.css";
+import * as axios from "axios";
+import photo from "../../images/user-icon.svg"
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.SetUsers([{
-            id: 1,
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSqFBoEXYUjcxgUBQxlwjRJlPMsMI5ZaKO2iHlInEl9giTFmZdvQ&s",
-            followed: true,
-            name: "Kolya",
-            location: {country: "Belarus", city: "Brest"},
-            status: "bike'n'roll"
-        },
-            {
-                id: 2,
-                img: "https://img.icons8.com/plasticine/2x/user.png",
-                followed: false,
-                name: "Pasha",
-                location: {country: "Belarus", city: "Kwakenburg"},
-                status: "ready to race"
-            },
-            {
-                id: 3,
-                img: "https://noguiltlife.com/wp-content/uploads/2019/11/baby-yoda-sipping-tea-meme-1-e1575123902614.jpg",
-                followed: true,
-                name: "Sanya",
-                location: {country: "Belarus", city: "Kwakenburg"},
-                status: "..."
-            },
-            {
-                id: 4,
-                img: "https://avatarfiles.alphacoders.com/651/thumb-65159.jpg",
-                followed: false,
-                name: "Sergey",
-                location: {country: "Belarus", city: "Brest"},
-                status: "Frontend-developer"
-            }])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(responce => {
+            props.SetUsers(responce.data.items)
+        })
     }
     return <div>
         {
             props.users.map(u => <div>
                 <span>
-                    <div><img className={s.img} src={u.img}/></div>
+                    <div><img className={s.img} src={u.photos.small != null ? u.photos.small : photo}/></div>
                 <div>
                     {u.followed ? <button onClick={() => {
                         props.Unfollow(u.id)
@@ -55,8 +28,8 @@ const Users = (props) => {
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
