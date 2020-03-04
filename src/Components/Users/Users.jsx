e0ff861d4a6dfa2122b61243283957b1ecca6fdc
Рома Хмарук <rkhmaruk4@gmail.com>
2020-03-04 09:45:28 +0300

    Used lifecycle method componentDidMount from ajax-request

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,8 +4,7 @@ import * as axios from "axios";
 import photo from "../../images/user-icon.svg"
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(responce => {
             this.props.SetUsers(responce.data.items)
         })
